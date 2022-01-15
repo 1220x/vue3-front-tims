90b27c889989cbc277c18205beac1fded01d7285
1220x <1960708821@qq.com>
2022-01-15 15:56:55 +0800

    feat: 安装element-plus；主页面骨架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,87 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
+    <div id="app" class="common-layout">
+        <el-container>
+            <el-header>XXX管理系统</el-header>
+            <el-container>
+                <el-aside>
+                    <el-menu>
+                        <el-menu-item
+                            v-for="{ menuName, menuRouteName } in menuConfig"
+                            :key="menuRouteName"
+                            @click="e => handleMenuClick(e, menuRouteName)"
+                        >{{ menuName }}</el-menu-item>
+                    </el-menu>
+                </el-aside>
+                <el-main>
+                    <transition>
+                        <router-view></router-view>
+                    </transition>
+                </el-main>
+            </el-container>
+            <el-footer>XXX管理系统</el-footer>
+        </el-container>
     </div>
 </template>
 
-<script>
+<script setup>
+import { reactive } from 'vue';
 
-export default {
-    name: 'App',
+let menuConfig = reactive([
+    { menuName: '1', menuRouteName: 'page1' },
+    { menuName: '2', menuRouteName: 'page2' },
+    { menuName: '3', menuRouteName: 'page3' },
+    { menuName: '4', menuRouteName: 'page4' },
+    { menuName: '5', menuRouteName: 'page5' },
+    { menuName: '6', menuRouteName: 'page6' },
+])
+
+function handleMenuClick(e, menuRouteName) {
+    if (this.$route.path.includes(menuRouteName)) return;
+
+    this.$router.push(menuRouteName);
 }
+
 </script>
 
 <style lang="scss">
+.common-layout {
+    .el-header,
+    .el-footer {
+        background-color: #b3c0d1;
+        color: #000;
+        text-align: center;
+        line-height: 60px;
+    }
 
+    .el-footer {
+        line-height: 60px;
+    }
+
+    .el-aside {
+        background-color: #d3dce6;
+        color: #000;
+        text-align: center;
+        line-height: 200px;
+    }
+
+    .el-main {
+        background-color: #e9eef3;
+        color: #000;
+        text-align: center;
+        line-height: 160px;
+    }
+
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
+
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/styles/common/index.scss';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(ElementPlus).mount('#app')
