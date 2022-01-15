62e34544e51a6088adcbde1862ad587899da570a
1220x <1960708821@qq.com>
2022-01-15 15:30:02 +0800

    feat: 全局样式文件

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div class="test-sass">
-        测试一下sass的安装
-        <div class="test-sass-div">
-            子元素
-        </div>
+    <div id="app">
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
     name: 'App',
-    components: {
-        HelloWorld
-    }
 }
 </script>
 
 <style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 
-.test-sass {
-    width: 200px;
-    height: 100px;
-    border: 1px solid #ff0000;
-    .test-sass-div {
-        color: #ff0000;
-    }
-}
 </style>
